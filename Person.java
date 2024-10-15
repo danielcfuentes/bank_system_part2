f3f30db506b651f844c2475bc2ccbbb20e2890e7
danielcfuentes <danielscollege04@gmail.com>
2024-10-15 14:35:16 -0600

    added person class for abstraction with attributes, getters/setters and other methods/

--- Person.java
+++ Person.java
@@ -1,3 +1,44 @@
-public class Person {
-    
+import java.util.List;
+
+public abstract class Person {
+    private String name;
+    private List<Account> accounts;
+    private String role;
+
+    // constrcutor no arg
+    public Person(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // method to check any account based on account number
+    public abstract List<Account> inquireAnyAccount(String accountNumber);
+
+    // method to pay another person
+    public void pay(Person receiver, double amount) {}
 }
