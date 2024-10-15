da57efff6ea29feb9bd9ed116bc6b7179fd7cd47
danielcfuentes <danielscollege04@gmail.com>
2024-10-15 14:39:23 -0600

    added basic strucutre for the TransactionLog class.

--- TransactionLog.java
+++ TransactionLog.java
@@ -1,3 +1,17 @@
+import java.util.List;
+
 public class TransactionLog {
-    
+    private List<String> currLogs;
+
+    // no arg constrouctor
+    public TransactionLog() {}
+
+    // getter
+    public List<String> getLogEntries() {
+        return currLogs;
+    }
+
+    public void logTransaction(String transaction) {}
+
+    public void exitUpdate() {}
 }
