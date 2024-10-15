372f2bb5f9b292b576d04002f0271114841caa99
danielcfuentes <danielscollege04@gmail.com>
2024-10-15 14:25:58 -0600

    added the methods for Run bank.

---
+++ RunBank.java
@@ -0,0 +1,10 @@
+public class RunBank {
+    public static void main(String[] args) {
+        
+    }
+
+    public String checkUserRole(Person person) {
+        // check user role
+        return null;
+    }
+}
