d29aa52e3c35773ef6f0d646604bcd95d4a52f00
danielcfuentes <danielscollege04@gmail.com>
2024-10-22 12:31:22 -0600

    added attributes and functionality to the Savings class. Only issue is that we ar ennot using th einterest rate, so we might have to create a function to generate new amount.

--- Savings.java
+++ Savings.java
@@ -1,3 +1,19 @@
-public class Savings {
-    
+public class Savings extends Account {
+    private double interestRate;
+
+    public Savings(){}
+
+    public void setInterestRate(double interestRate){
+        this.interestRate = interestRate;
+    }
+
+    public void tansfer(Account to, double amount){
+        if ( amount > 0 && balance >= amount){
+            this.withdraw(amount);
+            to.deposit(amount);
+            // need to log transaction
+        }else{
+            throw new IllegalArgumentException("Not Valid Transfer Amount");
+        }
+    }    
 }
