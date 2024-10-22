6c0d400716116a7065b95662ccb82ab56d0f7e0f
danielcfuentes <danielscollege04@gmail.com>
2024-10-22 12:16:29 -0600

    added the funcitionality for the abstract class of account with current attributes and functions. Need to do the log transaction function.

--- Account.java
+++ Account.java
@@ -1,3 +1,50 @@
-public class Account {
+public abstract class Account {
+    private double balance;
+    private String accountNumber;
     
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public Account(){}
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public double inquireBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            logTransaction("Deposit of $" + amount + " successful. New balance: $" + balance);
+        } else {
+            throw new IllegalArgumentException("Not Valid Amount: Deposit amount must be positive");
+        }
+    }
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            this.balance -= amount;
+            logTransaction("Withdrawal of $" + amount + " successful. New balance: $" + balance);
+        } else {
+            throw new IllegalArgumentException("Invalid withdrawal amount or insufficient funds");
+        }
+    }
+    
+    // log transaction to the transaction log
+    public void logTransaction(String transaction) {
+        return;
+    }
 }
