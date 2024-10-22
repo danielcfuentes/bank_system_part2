b128fa90aaf62ab741101194f40748c6e67e8c38
danielcfuentes <danielscollege04@gmail.com>
2024-10-22 12:41:40 -0600

    added the basic structure for the credit class. However we need to implement the borrow and pay. I also do not really understand what principle means. need help.

--- Credit.java
+++ Credit.java
@@ -1,3 +1,30 @@
-public class Credit {
-    
+public class Credit extends Account {
+    private double creditLimit;
+    private double principle;
+
+    public Credit(){}
+
+    public void setCreditLimit(double creditLimit){
+        this.creditLimit = creditLimit;
+    }
+
+    public void setPrinciple(double principle){
+        this.principle = principle;
+    }
+
+    public double getCreditLimit(){
+        return creditLimit;
+    }
+
+    public double getPrinciple(){
+        return principle;
+    }
+
+    public void borrow(double amount){
+        
+    }
+
+    public void pay(double amount){
+
+    }
 }
