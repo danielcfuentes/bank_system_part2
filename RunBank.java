4e228144adea5860e2a03679ed98249dd0e540ed
danielcfuentes <danielscollege04@gmail.com>
2024-10-22 14:00:04 -0600

    added the scanner to main class for the basic menu.

--- RunBank.java
+++ RunBank.java
@@ -1,10 +1,30 @@
-public class RunBank {
-    public static void main(String[] args) {
-        
-    }
+import java.util.Scanner;
 
-    public String checkUserRole(Person person) {
-        // check user role
-        return null;
+public class RunBank {
+
+    public static Scanner scanner = new Scanner(System.in); 
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("\nWelcome to El Paso Miners Bank");
+            System.out.println("1. Individual Customer");
+            System.out.println("2. Bank Manager");
+            System.out.println("Type 'EXIT' to quit");
+            
+            String choice = scanner.nextLine();
+            
+            if (choice.equalsIgnoreCase("EXIT")) {
+                // saveAndExit();
+                break;
+            }
+            
+            switch (choice) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
 }
