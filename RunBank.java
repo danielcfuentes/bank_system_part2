c1c9943693440fa145119ab1c24a05ca371015ed
Rogelio <rogeliolozano900@hotmail.com>
2024-10-22 16:21:38 -0600

    added javadoc comments to runBank.java

--- RunBank.java
+++ RunBank.java
@@ -1,8 +1,22 @@
+/**
+ * Start point for running the bank application.
+ */
 public class RunBank {
+
+    /**
+     * Main method to start the bank system.
+     * 
+     */
     public static void main(String[] args) {
         
     }
 
+    /**
+     * Checks the role of the specified user.
+     * 
+     * @param person the person whose role is to be checked
+     * @return the role of the user, or null if not found
+     */
     public String checkUserRole(Person person) {
         // check user role
         return null;
