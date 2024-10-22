af94c53cc54986861b99f9a77625ffdea3be5aaa
Rogelio <rogeliolozano900@hotmail.com>
2024-10-22 16:07:29 -0600

    added javadoc comments to checking.java

--- Checkings.java
+++ Checkings.java
@@ -1,10 +1,25 @@
+/**
+ * Represents a checking account with the ability to transfer funds.
+ */
 public class Checkings extends Account{
 
+    /**
+     * Initializes a Checkings account with the given account number and balance.
+     * 
+     * @param accountNumber the account identifier
+     * @param balance the initial balance
+     */
     public Checkings(String accountNumber, double balance){
         super(accountNumber, balance);
     }
-
-
+    
+    /**
+     * Transfers a specified amount to another account.
+     * 
+     * @param to the target account
+     * @param amount the amount to transfer
+     * @throws IllegalArgumentException if the amount is invalid
+     */
     public void tansfer(Account to, double amount){
         if ( amount > 0 && amount <= balance){
             this.withdraw(amount);
