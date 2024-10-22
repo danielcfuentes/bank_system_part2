fd6dd1350e524dfc0e2520faaf38dee90d26c09b
Rogelio <rogeliolozano900@hotmail.com>
2024-10-22 16:26:03 -0600

    added javadoc comments to savings.java

--- Savings.java
+++ Savings.java
@@ -1,8 +1,25 @@
+/**
+ * Represents a savings account with the ability to transfer funds to another account.
+ */
 public class Savings extends Account {
+
+    /**
+     * Initializes a Savings account with an account number and balance.
+     * 
+     * @param accountNumber the account identifier
+     * @param balance the initial balance
+     */
     public Savings(String accountNUmber, double balance){
         super(accountNUmber, balance);
     }
 
+    /**
+     * Transfers a specified amount to another account.
+     * 
+     * @param to the target account
+     * @param amount the amount to transfer
+     * @throws IllegalArgumentException if the transfer amount is invalid or insufficient funds
+     */
     public void tansfer(Account to, double amount){
         if ( amount > 0 && balance >= amount){
             this.withdraw(amount);
